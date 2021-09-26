c178f8223a2fdca9574a4d83f823309348088ebb
martynakr <martyna.krool@gmail.com>
2021-09-26 15:59:25 +1000

    modified mobile nav and added animations to the whole website

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ const menuLinkTwo = document.querySelector(".menu-link-two")
 const menuLinkThree = document.querySelector(".menu-link-three")
 const menuLinkFour = document.querySelector(".menu-link-four")
 
+const navItems = document.querySelectorAll(".menu-nav__item")
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -25,6 +27,7 @@ function toggleMenu() {
       nav.classList.add('open');
       menuNav.classList.add('open');
       bodFix.classList.add('open');
+      navItems.forEach(item => item.classList.add("open"))
     
       showMenu = true;
     } else {
@@ -32,6 +35,7 @@ function toggleMenu() {
       nav.classList.remove('open');
       menuNav.classList.remove('open');
       bodFix.classList.remove('open');
+      navItems.forEach(item => item.classList.remove("open"))
   
       showMenu = false;
     }
@@ -53,3 +57,36 @@ function toggleMenu() {
   // function moveCircle () {
   //   yellowCircle.classList.remove('open');
   // }
+
+
+  // fade ins
+
+
+
+const faders = document.querySelectorAll(".fade-in")
+const sliders = document.querySelectorAll(".slide-in")
+
+const appearOptions = {
+    rootMargin: "0px 0px -150px 0px",
+  };
+
+
+const apperaOnScroll = new IntersectionObserver(function(entries, apperaOnScroll) {
+entries.forEach(entry => {
+  if(!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.classList.add('appear');
+    apperaOnScroll.unobserve(entry.target);
+  }
+})
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+  apperaOnScroll.observe(fader)
+})
+
+sliders.forEach(slider => {
+  apperaOnScroll.observe(slider);
+})
